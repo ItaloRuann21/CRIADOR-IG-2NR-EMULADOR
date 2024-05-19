de91735cc2531b562ba1331b10baf6c561605419
ItaloRuann21 <italotavares751@gmail.com>
2024-05-18 21:46:19 -0300

    v6.9 - Add configuração de json

--- configuracoes_usuario.py
+++ configuracoes_usuario.py
@@ -1,39 +1,75 @@
-
-
-import shutil
+import json
+import os
 
 from mensagens.mensagens import mensagem_atencao, mensagem_sucesso
 
 
+def carregar_configuracoes():
+    if os.path.exists('./configuracoes.json'):
+        with open('./configuracoes.json', 'r') as file:
+            return json.load(file)
+    return {}
+
+
+def salvar_configuracoes(config):
+    with open('./configuracoes.json', 'w') as file:
+        json.dump(config, file, indent=4)
+
+
 def configuracao():
+    configuracoes_salvas = carregar_configuracoes()
 
     print('')
-    mensagem_atencao(' Digite a porta do emulador:')
-    User_porta = input('> ')
+    mensagem_atencao(
+        f' Digite a porta do emulador')
+    porta = input('> ')
     print('')
-    porta = str(User_porta)
-    mensagem_atencao(' Escolha o clonador:')
+    mensagem_atencao(
+        f' Escolha o clonador: [PADRAO: {configuracoes_salvas.get("clonador", "")}]')
     clonador = input(
-        '1- Varias Contas\n2- 2accounts\n3- Aplicativo Paralelo\n> ')
+        f'1- Varias Contas\n2- 2accounts\n3- Aplicativo Paralelo\n> ') or configuracoes_salvas.get("clonador", "")
     print('')
 
-    mensagem_atencao(' Escolha a VPN padrão:')
+    mensagem_atencao(
+        f' Escolha a VPN padrão: [PADRAO: {configuracoes_salvas.get("definir_vpn", "")}]')
     definir_vpn = input(
-        '1- SurfSharke\n2- Fast VPN Freedom\n3- Super VPN Unlimited Proxy\n4- AVG VPN\n5- Criação com VPN aleatória\n> ')
+        f'1- SurfSharke\n2- Fast VPN Freedom\n3- Super VPN Unlimited Proxy\n4- AVG VPN\n5- Criação com VPN aleatória\n> ') or configuracoes_salvas.get("definir_vpn", "")
     print('')
 
-    mensagem_atencao(' Criar quantas contas com mesmo número?')
-    quantidade_contas_por_numero = input('> ')
+    mensagem_atencao(
+        f' Criar quantas contas com o mesmo número? [PADRAO: {configuracoes_salvas.get("quantidade_contas_por_numero", "")}]')
+    quantidade_contas_por_numero = input(
+        f'> ') or configuracoes_salvas.get("quantidade_contas_por_numero", "")
     print('')
-    mensagem_atencao(' Gênero dos perfis:')
-    genero = input('1- Masculino\n2- Feminina\n> ')
+    mensagem_atencao(
+        f' Gênero dos perfis: [PADRAO: {configuracoes_salvas.get("genero", "")}]')
+    genero = input(
+        '1- Masculino\n2- Feminina\n> ') or configuracoes_salvas.get("genero", "")
     print('')
 
-    mensagem_atencao(' Defina em segundos a velocidade do bot:')
-    velocidade_bot = int(input('> '))
+    mensagem_atencao(
+        f' Defina em segundos a velocidade do bot: [PADRAO: {configuracoes_salvas.get("velocidade_bot", "")}]')
+    velocidade_bot = int(
+        input('> ') or configuracoes_salvas.get("velocidade_bot", ""))
     print('')
 
-    mensagem_atencao(' Defina qual opção você deseja:')
-    _2nr = input('1- Logar no 2NR via Gmail\n2- Criação de conta 2nr\n> ')
+    mensagem_atencao(
+        f' Defina qual opção você deseja: [PADRAO: {configuracoes_salvas.get("_2nr", "")}]')
+    _2nr = input(
+        '1- Logar no 2NR via Gmail\n2- Criação de conta 2nr\n> ') or configuracoes_salvas.get("_2nr", "")
+
+    # Salvar configurações em um dicionário
+    config = {
+        "clonador": clonador,
+        "definir_vpn": definir_vpn,
+        "quantidade_contas_por_numero": quantidade_contas_por_numero,
+        "genero": genero,
+        "velocidade_bot": velocidade_bot,
+        "_2nr": _2nr
+    }
+
+    # Salvar configurações em um arquivo JSON
+    salvar_configuracoes(config)
+    mensagem_sucesso('Configurações salvas com sucesso!')
 
     return porta, definir_vpn, quantidade_contas_por_numero, velocidade_bot, genero, _2nr, clonador
